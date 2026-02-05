e2d48b014422478e503fe13ffda450ea19e583f7
saurabhtripathi7 <saurabhkmtripathi@gmail.com>
2026-02-05 22:49:15 +0530

    feat: Configure CORS to allow specific origins and enable credentials

--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,15 @@ import userRouter from "./routes/userRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : [])
+  ],
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 app.use(clerkMiddleware());
